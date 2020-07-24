745379006b0e07a1f9c53bccf673f4bf0659cc40
iceBear67 <icebear67@sfclub.cc>
2020-07-24 13:22:36 +0800

    [Add] canBeOnGround getY and generating zombies  - Not debugged.

--- src/main/java/pw/illusion/zombietide/Main.java
+++ src/main/java/pw/illusion/zombietide/Main.java
@@ -2,23 +2,35 @@ package pw.illusion.zombietide;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
+import org.bukkit.util.Vector;
 import pw.illusion.randevent.EventManager;
 import pw.illusion.randevent.events.EventStart;
 
+import java.util.Objects;
 import java.util.Random;
 
 public class Main extends JavaPlugin implements Listener {
-
+    private static final Random random=new Random();
+    private boolean started=false;
+    private BukkitTask process;
     @Override
     public void onEnable() {
         getLogger().info("ZombieTide loaded successfully!");
-        EventManager.getInstance().registerRandEvent("zombie_tide", () -> new Random().nextInt(30) == 1 && getServer().getWorld(getConfig().getString("world")).getTime() >= getConfig().getInt("time"));
-        Bukkit.getPluginManager().registerEvents(this, this);
         saveDefaultConfig();
+        EventManager.getInstance().registerRandEvent("zombie_tide", () -> {
+            if(started) return false;
+            return (random.nextInt(30)==1 && Bukkit.getWorld(getConfig().getString("world")).getTime()>getConfig().getLong("time"));
+        });
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
 
     @Override
@@ -29,13 +41,63 @@ public class Main extends JavaPlugin implements Listener {
     @EventHandler
     void onRandomEvent(EventStart event) {
         if ("zombie_tide".equals(event.getEventName())) {
-            Bukkit.getOnlinePlayers().stream().filter(s -> getConfig().getString("world").equals(s.getWorld().getName()) && new Random().nextInt(10) == 1).forEach(this::summonZombies);
+            started=true;
+            process = Bukkit.getScheduler().runTaskTimer(this,()->{
+                World omgWorld=Bukkit.getWorld(Objects.requireNonNull(getConfig().getString("world")));
+                if(omgWorld.getTime()<getConfig().getLong("time")){
+                    started=false;
+                    process.cancel();
+                }
+                omgWorld.getPlayers().stream().filter(s->!s.isSleeping()).forEach(p->{
+                    summonZombies(p);
+                });
+            },0L,getConfig().getInt("interval")*20L);
         }
     }
 
     void summonZombies(Player player) {
         Location loc = player.getLocation();
-        loc.add(new Random().nextInt(40) - 20, 0, new Random().nextInt(40) - 20);
+        int debug=0;
+        do {
+            debug++;
+            loc.add(Vector.getRandom()).setY(getY(loc, player));
+        }
+        while(loc.distance(player.getLocation())<5 || loc.distance(player.getLocation())>15 && canBeOnGround(loc));
+        getLogger().info("DEBUGGING : loop for "+debug+" times!");
+        loc.getWorld().spawnEntity(loc, EntityType.ARMOR_STAND);
     }
+    double getY(Location loc,Player player){
+        if(loc.getBlock().getType()== Material.AIR && isHeadAir(loc)){
+            return loc.getY();
+        }
+        //else..
+        Location loc2=loc;
+        for(int i=0;i<=3;i++){ //search up..
+            loc2.setY(loc2.getY()+1);
+            if(loc2.getBlock().getType()==Material.AIR && isHeadAir(loc2)){
+                return loc2.getY();
+            }
+        }
+        loc2=loc;
+        for(int i=0;i<=3;i++){ //search down..
+            loc2.setY(loc2.getY()-1);
+            if(loc2.getBlock().getType()==Material.AIR && isHeadAir(loc2)){
+                return loc2.getY();
+            }
+        }
+        return loc.getY();
+    }
+    boolean isHeadAir(Location loc){
+        Location head=loc;
+        head.setY(head.getY()+1);
+        return head.getBlock().getType()==Material.AIR;
+    }
+    boolean canBeOnGround(Location loc){
+        Location ground=loc;
+        ground.setY(loc.getY()-1);
+        Material type=ground.getBlock().getType();
+        return type!=Material.AIR && !type.name().contains("GATE") && !type.name().contains("DOOR");
+    }
+
 
 }
