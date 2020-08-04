fe4bedb967d498c4c8b6f22c115370cda6c7079f
Yoooooory <youranpublic@gmail.com>
2020-08-04 23:35:16 +0800

    [Refact] all things that you can imagine
    [Refact] Almost all things in the main method.
    However, they haven't be tested at all.

--- src/main/java/pw/illusion/zombietide/Main.java
+++ src/main/java/pw/illusion/zombietide/Main.java
@@ -2,35 +2,39 @@ package pw.illusion.zombietide;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.World;
-import org.bukkit.block.Block;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
-import org.bukkit.util.Vector;
 import pw.illusion.randevent.EventManager;
 import pw.illusion.randevent.events.EventStart;
 
 import java.util.Objects;
 import java.util.Random;
 
+/**
+ * @author Yoooooory
+ */
 public class Main extends JavaPlugin implements Listener {
-    private static final Random random=new Random();
-    private boolean started=false;
-    private BukkitTask process;
+
+    private final Random random = new Random();
+    private BukkitTask process = null;
+    private final String world = Objects.requireNonNull(getConfig().getString("world"));
+
     @Override
     public void onEnable() {
-        getLogger().info("ZombieTide loaded successfully!");
         saveDefaultConfig();
         EventManager.getInstance().registerRandEvent("zombie_tide", () -> {
-            if(started) return false;
-            return (random.nextInt(30)==1 && Bukkit.getWorld(getConfig().getString("world")).getTime()>getConfig().getLong("time"));
+            if (process != null && !process.isCancelled()) return false;
+            return (random.nextInt(30) == 1 && Objects.requireNonNull(Bukkit.getWorld(world)).getTime() > getConfig().getLong("time"));
         });
         Bukkit.getPluginManager().registerEvents(this, this);
+        getLogger().info("ZombieTide loaded successfully!");
     }
 
     @Override
@@ -41,64 +45,62 @@ public class Main extends JavaPlugin implements Listener {
     @EventHandler
     void onRandomEvent(EventStart event) {
         if ("zombie_tide".equals(event.getEventName())) {
-            started=true;
-            process = Bukkit.getScheduler().runTaskTimer(this,()->{
-                World omgWorld=Bukkit.getWorld(Objects.requireNonNull(getConfig().getString("world")));
-                if(omgWorld.getTime()<getConfig().getLong("time")){
-                    started=false;
-                    process.cancel();
+            World omgWorld = Bukkit.getWorld(Objects.requireNonNull(getConfig().getString("world")));
+            assert omgWorld != null : String.format("Unknown world '%s'", getConfig().getString("world"));
+            process = new BukkitRunnable() {
+                @Override
+                public void run() {
+                    if (omgWorld.getTime() < getConfig().getLong("time")) {
+                        cancel();
+                    }
+                    omgWorld.getPlayers().forEach(s -> pinniganjuequxie(s));
                 }
-                omgWorld.getPlayers().stream().filter(s->!s.isSleeping()).forEach(p->{
-                    summonZombies(p);
-                });
-            },0L,getConfig().getInt("interval")*20L);
+            }.runTaskTimer(this, 0L, getConfig().getInt("interval") * 20L);
         }
     }
 
-    void summonZombies(Player player) {
-        Location loc = player.getLocation();
-        int debug=0;
-        long time=System.currentTimeMillis();
-        do {
-            debug++;
-            loc.add(Vector.getRandom()).setY(getY(loc, player));
-        }
-        while(loc.distance(player.getLocation())<5 || loc.distance(player.getLocation())>15 && canBeOnGround(loc));
-        getLogger().info("DEBUGGING : loop for "+debug+" times! Take "+(System.currentTimeMillis()-time)+"ms.");
-        loc.getWorld().spawnEntity(loc, EntityType.ARMOR_STAND);
-    }
-    double getY(Location loc,Player player){
-        if(loc.getBlock().getType()== Material.AIR && isHeadAir(loc)){
-            return loc.getY();
-        }
-        //else..
-        Location loc2=loc;
-        for(int i=0;i<=3;i++){ //search up..
-            loc2.setY(loc2.getY()+1);
-            if(loc2.getBlock().getType()==Material.AIR && isHeadAir(loc2)){
-                return loc2.getY();
+    /**
+     * Summon some zombies near a player.
+     * ( Method name: summonZombies )
+     *
+     * @param player The player.
+     */
+    void pinniganjuequxie(Player player) {
+        Location center = getValidLocation(player.getLocation().add(random.nextInt(20), 0, random.nextInt(20)));
+        if (Math.abs(center.getY() - player.getLocation().getY()) > 8) return;
+        World world = player.getWorld();
+        world.spawnEntity(center, EntityType.ZOMBIE);
+        // Try to summon zombies.
+        for (int i = 0; i < random.nextInt(20); i++) {
+            if (random.nextInt(4) != 1) continue;
+            Location loc = getValidLocation(center.clone().add(random.nextInt(6), 0, random.nextInt(6)));
+            if (Math.abs(loc.getY() - center.getY()) <= 4) {
+                world.spawnEntity(center, EntityType.ZOMBIE);
             }
         }
-        loc2=loc;
-        for(int i=0;i<=3;i++){ //search down..
-            loc2.setY(loc2.getY()-1);
-            if(loc2.getBlock().getType()==Material.AIR && isHeadAir(loc2)){
-                return loc2.getY();
-            }
-        }
-        return loc.getY();
-    }
-    boolean isHeadAir(Location loc){
-        Location head=loc;
-        head.setY(head.getY()+1);
-        return head.getBlock().getType()==Material.AIR;
-    }
-    boolean canBeOnGround(Location loc){
-        Location ground=loc;
-        ground.setY(loc.getY()-1);
-        Material type=ground.getBlock().getType();
-        return type!=Material.AIR && !type.name().contains("GATE") && !type.name().contains("DOOR");
+        player.playSound(player.getLocation(), Sound.ENTITY_BLAZE_SHOOT, Float.MAX_VALUE, 1);
+        player.sendTitle("", "§r一个大波僵尸正在接近", 2, 60, 15);
     }
 
+    Location getValidLocation(Location loc) {
+        // Magic code, don't delete! (神奇代码 勿删)
+        if (isLocationEmpty(loc) && isLocationEmpty(loc.clone().add(0, 1, 0))) {
+            // A valid location, but may be floating.
+            do {
+                loc.add(0, -1, 0);
+            } while (isLocationEmpty(loc));
+            return loc.add(0, 1, 0);
+        } else {
+            // An invalid location.
+            do {
+                loc.add(0, 1, 0);
+            } while (!isLocationEmpty(loc));
+            return loc;
+        }
+    }
+
+    boolean isLocationEmpty(Location loc) {
+        return loc.getBlock().isEmpty();
+    }
 
 }
